019c2641d98b3a381a9549e31dc8ec0136fe3888
Wambuilucy <wambuilucie99@gmail.com>
2020-11-03 17:44:39 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# SafeSide
+
+Search for weather alerts and weather conditions in any city !
+
+2020
+
+Built by Wambuilucy
+
+## Nature
+SafeSide is an app built using django.
+
+### Problem Statement
+
+Mother nature can be extreem sometimes and the climatic conditions can be life threatening. However, weather is very precitable and with information and proper planning , we are able to keep ourselves on the safe side.
+
+### Solution
+With Safeside, users are able to :
+
+get the actual weather conditions of any city in the world
+get weather alerts of extreem weather conditions such as snowfall, flash floods,heavy rainfall etc Thus enabling them to make life saving decisions such as evacuation
+
+### Setup
+Clone this repo to your desktop
+run this in your terminal "git remove set-url" if you wish to have it into your repository
+create a directory for this project
+run "pip install" in your terminal to install python packages and "pip update" to update the packages.
+run "atom ." in you terminal
+#### Bugs
+No known bugs.
+
+## Technologies
+Django
+Python3.6
+Javascript
+Bootstrap4
+Intallations
+Django
+Bootstrap4
+Requests
+Pprint
+### APIs
+Weather alerts api
+Weather conditions api
+#### References
+Statistics of death by floods in Kenya 2020
+Floods and drought are the deadliest natural phenomenon
+#### Support and contact details
+For feedback , complaints or queries : email: wambuilucie@gmail.com , or call : 0721105540
+
+#### License
+This project is licensed under the terms of the MIT license.
---
+++ safeapp/views.py
@@ -0,0 +1,59 @@
+from django.shortcuts import render,redirect,HttpResponseRedirect
+import requests
+import json
+import urllib.request
+
+
+def home(request):
+    data={}
+    data={}
+    if request.method == 'POST':
+        
+    # Try block to catch 
+        city = request.POST['city']
+
+        try:
+            # int('sy')
+            
+            source = requests.get('https://openweathermap.org/data/2.5/weather?q='+city+'&appid=b6907d289e10d714a6e88b30761fae22')
+            if not source.status_code//100  == 2:
+            
+                return redirect('error')  
+            source2 = requests.get('https://weather.cit.api.here.com/weather/1.0/report.json?product=alerts&name='+city+'&app_id=DemoAppId01082013GAL&app_code=AJKnXv84fjrb0KIHawS0Tg')
+
+    # converting JSON data to a dictionary 
+            if source.status_code == 200 and  source2.status_code == 200:
+                list_of_data = json.loads(source.content)
+                list_of_data2 = json.loads(source2.content)
+                print(type(list_of_data),'============================')
+                data = {
+                "country_code": str(list_of_data['sys']['country']), 
+                "coordinate": str(list_of_data['coord']['lon']) + ' '
+                        + str(list_of_data['coord']['lat']), 
+                "temp": str(list_of_data['main']['temp']) + 'k', 
+                "pressure": str(list_of_data['main']['pressure']), 
+                "humidity": str(list_of_data['main']['humidity']), 
+                }
+                if list_of_data2['alerts']['alerts'] == []:
+                            data2 = {
+                                "city": str(list_of_data2['alerts']['city']),
+                                "alerts": "Weather is not extreem in " + city
+
+                            }
+            # print(list_of_data2)
+                else:
+                    data2 = {
+                        "city": str(list_of_data2['alerts']['city']),
+                        "alerts": str(list_of_data2['alerts']['alerts'][0]['description'] + '. Stay safe')
+
+                    }                
+        except requests.exceptions.HTTPError as err:
+            print('------------')
+            
+        except requests.exceptions.RequestException as e:
+            print(e)
+
+    return render(request,'weather/home.html',locals())
+
+def error(request):
+    return render(request, 'weather/500.html')
